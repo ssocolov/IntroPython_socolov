eab28c3c31a9062bd6efb9561cfe8c991d6e6e06
ssocolov <favouriteseason2006@gmail.com>
2021-10-02 13:10:15 +0300

    DZ_lesson2_dorabotano

--- temp.py
+++ temp.py
@@ -1,8 +1,6 @@
-
 # num = int(input("введите целое число"))
 # result = "четное" if num%2==0 else "нечетное число"
 # print("Это " +result+" число")
-
 
 
 # from random import randint
@@ -16,6 +14,28 @@
 #
 # print("Выпало число:", case, "Результат:", result)
 
-value = 99
-new_value = value // 2 if value > 100 else - value
-print(new_value)
+# value = 99
+# new_value = value // 2 if value > 100 else - value
+# print(new_value)
+
+
+# 6 (доработано)
+#
+# my_str = "Sergey"
+# result: str = (my_str)    # нужно ли?
+# if len(my_str) < 5:
+#     result = (my_str) * 2
+# elif len(my_str) >= 5:
+#     result = (my_str)
+# print(result)
+
+###################################################################################
+# 7 (доработано)
+
+# my_str = "Serg"
+# result: str = (my_str)    # нужно ли?
+# if len(my_str) < 5:
+#     result = (my_str + my_str[::-1])
+# elif len(my_str) >= 5:
+#     result = (my_str)
+# print(result)
