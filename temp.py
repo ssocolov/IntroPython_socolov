f30b015d7d633e77c534ab2f8bf0df422e6bae28
ssocolov <favouriteseason2006@gmail.com>
2021-10-22 00:54:55 +0300

    DZ_lesson5_starting

--- temp.py
+++ temp.py
@@ -1,3 +1,152 @@
+#############Lesson5_1
+
+# https://pythonru.com/osnovy/python-dict
+
+# # Словари, методы словарей
+# # модули
+# # функции
+#
+# # ######################### словари
+# my_dict = {"name": "John", "age": 23}  # изменяемый, итерируемый тип данных. Порядок ключей не гарантируется!
+# print(my_dict, type(my_dict))
+# print(my_dict["name"])
+# print(my_dict.get(111))
+#
+# ########################### конструирование сложных словарей
+# address = {"city": "Dnipro", "street": "Polya", "ZIP": 49000}
+# skils = {"hard": ["python", "html", "DB", "C++"], "soft": []}
+# person = {"name": "John", "age": 23, "skils": skils}
+# person_datails = {"sex": "Male", "address": address}
+#
+# ######################### описание структуры данных с помощью словарей
+#
+# person = {"name": "John",
+#           "age": 23,
+#           "sex": "Male",
+#           "address": {"city": "Dnipro",
+#                       "street": "Polya",
+#                       "ZIP": 49000
+#                       },
+#           "skils": {"hard": ["python", "html", "DB", "C++"],
+#                     "soft": []
+#                     }
+#           }
+#
+# ######################################### конструирование словаря через метод update
+# new_person = dict()
+# new_person.update(person)
+# new_person.update(person_datails)
+#
+# new_person = {**person_datails, **person}
+# print(new_person["address"]["city"])
+# new_person["skils"]["hard"].append("JS")
+# print(new_person)
+# person.update(person_datails)
+# print(person)
+# person["sex"] = "Male"
+# print(person)
+#
+# ######################################## приведение к типу dict
+# my_tuple = (("name", "John"), ("age", 23))
+# my_list = [("name", "John"), ["age", 23]]
+# person = dict(my_list)
+# print(person)
+#
+#
+# ########################################## циклы и dict
+#
+# my_dict = {1: 11, (1, 2, 3): "test", "1": "TEST"}
+# print(11 in my_dict)  # in  "смотрит" только в ключи
+#
+# print(my_dict.keys())  # dict_keys - "почти" список ключей ))
+# keys = list(my_dict.keys())
+# values = my_dict.values()   #dict_values - "почти" список значений
+# items = my_dict.items()
+# print(items)
+#
+#
+# my_dict = {"val_1": 12, "val_2": 24, "val_3": 6, "val_4": 58}
+#
+# for key in my_dict:  # "идет" ТОЛЬКО ПО КЛЮЧАМ
+#     print(key, my_dict[key])
+#
+# res_values = []
+# res_keys = []
+# for key, value in my_dict.items():
+#     if value > 10:
+#         res_values.append(value)
+#         res_keys.append(key)
+#
+# print(res_values)
+# print(res_keys)
+
+###### lesson5_2
+
+
+# import random
+#
+#
+# # функции
+#
+# def create_random_int_number_list(len_list=5):
+#     numbers = [random.randint(1, 10) for _ in range(len_list)]
+#     return numbers
+#
+#
+# def print_dict(some_dict):  # ВСЕ ЧТО ИСПОЛЬЗУЕМ - ПЕРЕДАЕМ В ФУНКЦИЮ!!!
+#     for key, value in some_dict.items():
+#         print(f"{key}: {value}")
+#
+#
+# my_dict = {"val_1": 12, "val_2": 24, "val_3": 6, "val_4": 58}
+# person = {"name": "John", "age": 23, "sex": "Male", }
+#
+# # print(person)
+# # print_dict(person)
+# # print_dict(some_dict=my_dict)
+# len_list = random.randint(10, 20)
+# result = create_random_int_number_list()
+# print(result)
+#
+# # использование модулей, пакетов, библиотек
+# import random  # импорт всего модуля
+# # from random import randint  # импорт конкретной функции
+# from string import ascii_lowercase as alphabet
+
+# value = random.randint(1, 10)
+# print(value)
+# my_list = ["True", "False", "Unknown"]
+# my_choice = random.choice(my_list)
+# print(my_choice)
+
+# alphabet_list = list(alphabet)
+# print(alphabet_list, type(alphabet_list))
+#
+# random.shuffle(alphabet_list)
+# print(alphabet_list)
+
+
+# my_temp_dict = {11: 1, 12: 2, 13: 3}
+# print(my_temp_dict)
+#
+# if len(my_temp_dict.values()) == len(set(my_temp_dict.values())) :
+#     temp_revers_dict = {value: key for key, value in my_temp_dict.items()}
+#     print(temp_revers_dict)
+
+# my_str = "qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqa"
+# res = []
+# for symb in set(my_str):
+#     if my_str.count(symb) == 1:
+#        res.append(symb)
+# print(res)
+
+
+
+
+
+#######################################################################################################################
+#######################################################################################################################
+
 # Строки, списки, циклы. Генераторы списков, множества
 # Сложение строк
 # s1 = "spam"
